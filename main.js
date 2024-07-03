93fd616c68b317dbd7da27152b92ba0138d1ffd0
afshan-ahmar <160451492+afshan-ahmar@users.noreply.github.com>
2024-07-03 04:00:29 -0700

    THIS IS MY EXAM PREPARATION STEP 00 TO STEP 02

---
+++ main.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+// HELLO WORLD
+let message = "HELLO, WORLD!";
+console.log(message);
+//JSON_objects
+// JSON_OBJECT
+function user(std_id, name, age, gender, course) {
+    return {
+        STUDENT_ID: std_id,
+        NAME: name,
+        AGE: age,
+        GENDER: gender,
+        COURSE: course
+    };
+}
+console.log(user(12723, "AFSHAN AHMAR", 34, "female", "GIAIC"));
+//SYNTAX ERROR
+lett;
+message = "Pakistan";
+console.logg(message);
+//TYPE ERROR
+//introduce a syntax error
+let a = 7;
+let b = "iraq";
+let c = true;
+console.log(iraq, "6", false, ");
+//ASSIGNABILITY ERROR//NUMBER NOT ASSIGNED
+, 
+//ASSIGNABILITY ERROR//NUMBER NOT ASSIGNED
+let, msg = "good morning");
+msg = 7;
+console.log(msg);
+//never use two variable with same name
+let myName = "afshan";
+let myName = "ifra";
+console.log(myName);
+export {};
